64c8d7ccd02e41d225d98318a51f16c0d9bbcd11
Nour Aoude <naoude@gmail.com>
2022-04-25 23:29:50 +0300

    Complete 03/reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = [];
+    for (let i = 0; i < string.length; i++) {
+        stringArray[i] = string.charAt(i);
+    }
+    newString = '';
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        newString += stringArray[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
